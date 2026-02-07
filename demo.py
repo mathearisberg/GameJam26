d5e05cd783e57ebebf410b189796265a9ade1aec
Niklas Gjerde <niklasgjerde240@gmail.com>
2026-02-07 13:09:17 +0100

    game speeds up while playing

--- demo.py
+++ demo.py
@@ -18,6 +18,7 @@ pygame.display.set_caption("Growing Plant")
 clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 32)
 
+
 # --------------------
 # GAME SETUP
 # --------------------
@@ -36,6 +37,8 @@ obstacles = pygame.sprite.Group()
 suns = pygame.sprite.Group()
 
 player = reset_game()
+game_time = 0
+current_speed = BASE_SPEED
 game_over = False
 score = 0
 
@@ -44,6 +47,13 @@ score = 0
 # --------------------
 while True:
     dt = clock.tick(FPS)
+
+    if not game_over:
+        game_time += dt
+        current_speed = min(
+            BASE_SPEED + game_time * SPEED_INCREASE_RATE,
+            MAX_SPEED
+        )
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -59,12 +69,12 @@ while True:
                 score = 0
 
         if event.type == SPAWN_OBSTACLE_EVENT and not game_over:
-            g = Gardener()
+            g = Gardener(current_speed)
             obstacles.add(g)
             all_sprites.add(g)
 
         if event.type == SPAWN_SUN_EVENT and not game_over:
-            s = Sun()
+            s = Sun(current_speed)
             suns.add(s)
             all_sprites.add(s)
 
